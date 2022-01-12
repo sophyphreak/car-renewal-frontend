a73bd77d6aa199fc13c372977789c5b0fffe1093
Andrew Horn <arhorn@smcm.edu>
2022-01-11 20:32:30 -0600

    updated name of site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Vehicle Registration Renewal Locations`,
+    title: `Harris County Vehicle Registration Renewal Locations`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
